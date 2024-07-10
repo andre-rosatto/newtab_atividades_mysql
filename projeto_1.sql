62034b91454f45f0bb840688b6dbe51993785985
André Rosatto <linus1178@gmail.com>
2024-07-10 18:00:21 -0300

    'refatoração'

--- projeto_1.sql
+++ projeto_1.sql
@@ -5,31 +5,31 @@ USE projeto_1;
 
 -- Criar a tabela "transacao" com os seguintes campos: Id, Tipo, Nome, Valor, Data de cadastro
 /*
-DROP TABLE IF EXISTS transacao;
-CREATE TABLE transacao(
+DROP TABLE IF EXISTS transacoes;
+CREATE TABLE transacoes(
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	tipo VARCHAR(6) CHECK(tipo IN('compra', 'venda')) NOT NULL DEFAULT 'venda',
-	nome VARCHAR(100) NOT NULL DEFAULT '',
+	tipo ENUM('compra', 'venda') NOT NULL DEFAULT 'venda',
+	nome VARCHAR(200) NOT NULL DEFAULT '',
 	valor DECIMAL(7, 2) NOT NULL DEFAULT 0,
-	data_cadastro DATETIME NOT NULL
+	data_cadastro DATETIME NOT NULL DEFAULT NOW()
 );
 */
 
 -- Criar uma query que insira uma transação com os campos tipo, nome, valor e data de cadastro.
 /*
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('venda', '2000 parafusos', 143.78, NOW());
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('compra', 'Máquina de café', -699.99, NOW());
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('compra', 'Teclado de computador', -83.5, NOW());
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('venda', 'Escada de metal 6m', 258.99, NOW());
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('venda', 'Furadeira', 459.98, NOW());
-INSERT INTO transacao(tipo, nome, valor, data_cadastro) VALUES ('venda', 'Chave de fenda', 18.50, NOW());
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('venda', '2000 parafusos', 143.78);
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('compra', 'Máquina de café', -699.99);
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('compra', 'Teclado de computador', -83.5);
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('venda', 'Escada de metal 6m', 258.99);
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('venda', 'Furadeira', 459.98);
+INSERT INTO transacoes(tipo, nome, valor) VALUES ('venda', 'Chave de fenda', 18.50);
 */
 
 -- Criar uma query que retorne todas as transações cadastradas em ordem crescente da data de cadastro.
--- SELECT * FROM transacao ORDER BY data_cadastro;
+-- SELECT id, tipo, nome, valor, data_cadastro FROM transacoes ORDER BY data_cadastro ASC;
 
 -- Criar uma query que retorne o saldo acumulado considerando todas as transações cadastradas.
--- SELECT SUM(valor) AS 'Saldo' FROM transacao;
+-- SELECT SUM(valor) AS 'saldo' FROM transacoes;
 
 -- Criar uma query que exclua todas as transações cadastradas.
--- DELETE FROM transacao;
+-- DELETE FROM transacoes;
--- projeto_2.sql
+++ projeto_2.sql
@@ -2,22 +2,22 @@ USE projeto_2;
 
 -- Criar a tabela "usuario" com os seguintes campos: Id, Nome, Imagem, Nome de usuario, Data de cadastro
 /*
-DROP TABLE IF EXISTS usuario;
-CREATE TABLE usuario (
-	id_usuario INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(100) NOT NULL,
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(200) NOT NULL,
 	imagem VARCHAR(255) NOT NULL,
-	nome_usuario VARCHAR(100) NOT NULL,
+	nome_usuario VARCHAR(200) NOT NULL,
 	data_cadastro DATE NOT NULL
 );
 */
 
 -- Criar a tabela "cartao" com os seguintes campos: Id, Numero, Cvv, Data de expiracao, Valido, Data de cadastro
 /*
-DROP TABLE IF EXISTS cartao;
-CREATE TABLE cartao(
-	id_cartao INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	numero VARCHAR(19) NOT NULL,
+DROP TABLE IF EXISTS cartoes;
+CREATE TABLE cartoes(
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	numero VARCHAR(30) NOT NULL,
 	cvv VARCHAR(3) NOT NULL,
 	data_expiracao DATE NOT NULL,
 	valido BOOLEAN NOT NULL DEFAULT TRUE,
@@ -27,49 +27,54 @@ CREATE TABLE cartao(
 
 -- Criar a tabela "transacao" com os seguintes campos: Id, Id_usuario, Id_cartao, Valor, Data de cadastro
 /*
-DROP TABLE IF EXISTS transacao;
-CREATE TABLE transacao(
-	id_transacao INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT UNSIGNED NOT NULL,
-	id_cartao INT UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS transacoes;
+CREATE TABLE transacoes(
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	usuario_id INT UNSIGNED NOT NULL,
+	cartao_id INT UNSIGNED NOT NULL,
 	valor DECIMAL(7, 2) NOT NULL,
 	data_cadastro DATE NOT NULL,
-	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-	FOREIGN KEY (id_cartao) REFERENCES cartao(id_cartao)
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+	FOREIGN KEY (cartao_id) REFERENCES cartoes(id)
 );
 */
 
 -- Criar uma query que insira um usuário com os campos nome, imagem, nome de usuário e data de cadastro.
--- INSERT INTO usuario(nome, imagem, nome_usuario, data_cadastro) VALUES('Marcos Almeida', 'm_almeida_pic.jpg', 'm_almeida', '2023-10-15');
--- INSERT INTO usuario(nome, imagem, nome_usuario, data_cadastro) VALUES('Alexandra Maria Souza', 'ams_pic.jpg', 'amsouza', '2021-02-01');
--- INSERT INTO usuario(nome, imagem, nome_usuario, data_cadastro) VALUES('Ana Clara Valente', 'clara_valente_foto_perfil.jpg', 'c_valente', '2024-05-29');
--- INSERT INTO usuario(nome, imagem, nome_usuario, data_cadastro) VALUES('Sérgio Natalino Albuquerque', 'sergio_foto.jpg', 'sergio_albuquerque', '2020-12-25');
+/*
+INSERT INTO usuarios(nome, imagem, nome_usuario, data_cadastro) VALUES('Marcos Almeida', 'm_almeida_pic.jpg', 'm_almeida', '2023-10-15');
+INSERT INTO usuarios(nome, imagem, nome_usuario, data_cadastro) VALUES('Alexandra Maria Souza', 'ams_pic.jpg', 'amsouza', '2021-02-01');
+INSERT INTO usuarios(nome, imagem, nome_usuario, data_cadastro) VALUES('Ana Clara Valente', 'clara_valente_foto_perfil.jpg', 'c_valente', '2024-05-29');
+INSERT INTO usuarios(nome, imagem, nome_usuario, data_cadastro) VALUES('Sérgio Natalino Albuquerque', 'sergio_foto.jpg', 'sergio_albuquerque', '2020-12-25');
+*/
 
 -- Criar uma query que retorne todos os usuários cadastrados em ordem alfabética.
--- SELECT * FROM usuario ORDER BY nome;
+-- SELECT nome, imagem, nome_usuario, data_cadastro FROM usuarios ORDER BY nome ASC;
 
 -- Criar uma query que insira um cartão com os campos número, cvv, data de expiração, válido e data de cadastro.
 /*
-INSERT INTO cartao(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('1234 5678 9012 3456', '987', '2029-06-01', TRUE, '2023-11-02');
-INSERT INTO cartao(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('4004 8926 5026 6438', '174', '2028-08-01', TRUE, '2024-01-01');
-INSERT INTO cartao(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('5416 5383 9027 5234', '942', '2022-01-01', FALSE, '2018-05-23');
-INSERT INTO cartao(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('0346 8824 3366 5191', '685', '2021-12-01', FALSE, '2019-12-01');
-INSERT INTO cartao(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('0601 1201 7988 8769', '564', '2027-09-01', TRUE, '2022-09-16');
+INSERT INTO cartoes(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('1234 5678 9012 3456', '987', '2029-06-01', TRUE, '2023-11-02');
+INSERT INTO cartoes(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('4004 8926 5026 6438', '174', '2028-08-01', TRUE, '2024-01-01');
+INSERT INTO cartoes(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('5416 5383 9027 5234', '942', '2022-01-01', FALSE, '2018-05-23');
+INSERT INTO cartoes(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('0346 8824 3366 5191', '685', '2021-12-01', FALSE, '2019-12-01');
+INSERT INTO cartoes(numero, cvv, data_expiracao, valido, data_cadastro) VALUES('0601 1201 7988 8769', '564', '2027-09-01', TRUE, '2022-09-16');
 */
 
 -- Criar uma query que retorne apenas os cartões válidos em ordem crescente da data de cadastro.
--- SELECT * FROM cartao WHERE valido ORDER BY data_cadastro;
+-- SELECT numero, cvv, data_expiracao, valido, data_cadastro FROM cartoes WHERE valido ORDER BY data_cadastro ASC;
 
 -- Criar uma query que insira uma transação com os campos id_usuario, id_cartao, valor e data de cadastro.
 /*
-INSERT INTO transacao(id_usuario, id_cartao, valor, data_cadastro) VALUES(2, 1, 229.90, '2024-02-17');
-INSERT INTO transacao(id_usuario, id_cartao, valor, data_cadastro) VALUES(2, 1, 229.90, '2024-02-17');
-INSERT INTO transacao(id_usuario, id_cartao, valor, data_cadastro) VALUES(1, 2, 79, '2024-01-30');
-INSERT INTO transacao(id_usuario, id_cartao, valor, data_cadastro) VALUES(1, 2, 723.75, '2023-09-10');
-INSERT INTO transacao(id_usuario, id_cartao, valor, data_cadastro) VALUES(3, 5, 332.99, '2022-07-29');
+INSERT INTO transacoes(usuario_id, cartao_id, valor, data_cadastro) VALUES(2, 1, 229.90, '2024-02-17');
+INSERT INTO transacoes(usuario_id, cartao_id, valor, data_cadastro) VALUES(2, 1, 229.90, '2024-02-17');
+INSERT INTO transacoes(usuario_id, cartao_id, valor, data_cadastro) VALUES(1, 2, 79, '2024-01-30');
+INSERT INTO transacoes(usuario_id, cartao_id, valor, data_cadastro) VALUES(1, 2, 723.75, '2023-09-10');
+INSERT INTO transacoes(usuario_id, cartao_id, valor, data_cadastro) VALUES(3, 5, 332.99, '2022-07-29');
 */
 
 -- Criar uma query que retorne todas as transações cadastradas em ordem decrescente da data de cadastro, contendo os dados do usuário e cartão, não apenas seus IDs.
-SELECT * FROM transacao T, usuario U, cartao C
-WHERE T.id_usuario = U.id_usuario AND T.id_cartao = C.id_cartao
+/*
+SELECT T.id, T.valor, T.data_cadastro, U.id AS usuario_id, U.nome, U.imagem, U.imagem, U.data_cadastro AS usuario_data_cadastro, C.id AS cartao_id, C.numero, C.cvv, C.data_expiracao, C.valido, C.data_cadastro AS cartao_data_cadastro FROM transacoes T
+INNER JOIN usuarios U ON T.usuario_id = U.id
+INNER JOIN cartoes C ON T.cartao_id = C.id
 ORDER BY T.data_cadastro DESC;
+*/
